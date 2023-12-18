5d3bd8ca9130d3795faf5b1bdd64a504d54f83f2
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2023-12-18 14:16:20 -0300

    polybar: add battery indicator; nvim: add trouble plugin

--- nvim/lua/custom/plugins/formatter.lua
+++ nvim/lua/custom/plugins/formatter.lua
@@ -15,6 +15,7 @@ return {
         yaml = { "prettier" },
         markdown = { "prettier" },
         lua = { "stylua" },
+        go = { "gofmt" }
       },
       format_on_save = {
         lsp_fallback = true,
