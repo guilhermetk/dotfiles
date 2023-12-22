5aad7c0218b14b8d5743ba7da42961b0ba3de61c
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2023-12-22 11:47:18 -0300

    rose-pine colorcheme in kitty

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,6 +1,14 @@
 return {
     { "Mofiqul/dracula.nvim" },
-    { 'rose-pine/neovim',    name = 'rose-pine' },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true
+            })
+        end
+    },
     {
         "LazyVim/LazyVim",
         opts = {
