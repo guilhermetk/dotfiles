538d49e34c6e88e6d97188b75419fc8cd5acea4e
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-01-16 09:38:27 -0300

    restructure neovim plugins in separeted files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set relativenumber")
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,32 +9,8 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
-local plugins = {
-  { 'rose-pine/neovim', name = 'rose-pine' },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins, opts)
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+require("vim-options")
+require("lazy").setup('plugins')
 
-local configs = require('nvim-treesitter.configs')
-configs.setup({
-  ensure_installed = {'lua','javascript','go'},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-require("rose-pine").setup()
-vim.cmd.colorscheme "rose-pine-moon"
