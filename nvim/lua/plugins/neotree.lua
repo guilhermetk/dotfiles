49c94dc220e81dadfa57f1632ba550fc913c3a73
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-01-17 22:35:51 -0300

    added: neotree, lsp configuration, noice, which-key, lualine, autoclose, indent-blankline

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,6 +8,9 @@ return {
       "MunifTanjim/nui.nvim",
     },
     config = function()
+      require("neo-tree").setup({
+        hijack_netrw_behavior = "open_default"
+      })
       vim.keymap.set('n', '<leader>e', ':Neotree toggle reveal left<CR>',{})
     end
   }
