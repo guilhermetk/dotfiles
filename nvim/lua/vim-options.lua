47fbcc70a84f2c53c4723c854a2ec24b0f596725
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-01-25 10:28:27 -0300

    several improvements
    - i3: added keymap to change keyboard layout
    - kitty: updated font size
    - btop: added config
    - nvim: added harpoon, highlight on yank

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,18 +2,19 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
 vim.cmd("set relativenumber")
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.o.hlsearch = false
 
- -- [[ Highlight on yank ]]
+-- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
