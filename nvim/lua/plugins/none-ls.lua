07b043a969a2aa8e71cfee4ca7941d056c0300b1
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-02-19 16:21:30 -0300

    change js/ts linting from eslint_d to eslint-lsp

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,17 @@
 return {
-	'nvimtools/none-ls.nvim',
-	config = function()
-		local null_ls = require 'null-ls'
-		null_ls.setup {
-			sources = {
-				-- formatting
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				-- diagnostics
-				null_ls.builtins.diagnostics.eslint_d,
-				-- code actions
-				null_ls.builtins.code_actions.eslint_d,
-			},
-		}
-	end,
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        -- formatting
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        -- diagnostics
+        null_ls.builtins.diagnostics.eslint,
+        -- code actions
+        null_ls.builtins.code_actions.eslint,
+      },
+    }
+  end,
 }
