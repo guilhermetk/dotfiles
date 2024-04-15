ea66052376ea2b425ed932bfe2fc813d00c777b1
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-04-14 21:01:01 -0300

    feat: so many things

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.beautysh,
         -- diagnostics
+        null_ls.builtins.diagnostics.golangci_lint,
         -- null_ls.builtins.diagnostics.eslint,
         -- code actions
         -- null_ls.builtins.code_actions.eslint,
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -92,6 +92,7 @@ return {
     vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
     vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+    vim.keymap.set('n', '<leader>gs', require('telescope.builtin').git_status, { desc = 'Search [G]it [S]tatus' })
     vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
@@ -102,5 +103,8 @@ return {
     vim.keymap.set('n', '<leader>sn', function()
       require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
+    vim.keymap.set('n', '<leader>sff', function()
+      require('telescope.builtin').find_files { hidden = true}
+    end, { desc = '[S]earch hidden [F]iles' })
   end,
 }
