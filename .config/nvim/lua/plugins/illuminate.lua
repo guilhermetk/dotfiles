cc7556e9c5fb957cec01ea01e248772d8a6840a2
Guilherme Tiscoski <guilherme.tiscoski@asksuite.com>
2024-05-23 08:57:00 -0300

    fix: illuminate background color

--- .config/nvim/lua/plugins/illuminate.lua
+++ .config/nvim/lua/plugins/illuminate.lua
@@ -6,9 +6,9 @@ return {
       require('illuminate').configure {
         under_cursor = false,
       }
-      vim.api.nvim_command [[ hi IlluminatedWordText gui=NONE guibg='#817c9c' guifg='#e0def4']]
-      vim.api.nvim_command [[ hi IlluminatedWordRead gui=NONE guibg='#817c9c' guifg='#e0def4']]
-      vim.api.nvim_command [[ hi IlluminatedWordWrite gui=NONE guibg='#817c9c' guifg='#e0def4']]
+      vim.api.nvim_command [[ hi IlluminatedWordText gui=NONE guibg='#eb6f92' guifg='#e0def4']]
+      vim.api.nvim_command [[ hi IlluminatedWordRead gui=NONE guibg='#eb6f92' guifg='#e0def4']]
+      vim.api.nvim_command [[ hi IlluminatedWordWrite gui=NONE guibg='#eb6f92' guifg='#e0def4']]
     end,
   },
 }
