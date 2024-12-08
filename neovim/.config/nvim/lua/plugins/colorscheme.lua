f5a74ab920df790b74c0f30edf510de7777ffb37
Guilherme Tiscoski <guilhermetiscoski@Guilhermes-MacBook-Pro.local>
2024-12-08 09:44:44 -0600

    fix: disable italic style on rose pine theme

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,10 @@ return {
     config = function()
       require('rose-pine').setup({
         disable_background = true,
-        disable_float_background = true
+        disable_float_background = true,
+        styles = {
+          italic = false,
+        }
       })
       vim.cmd 'colorscheme rose-pine'
     end,
